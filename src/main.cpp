fbb56cbbe0ee91a4b1dcf36ebbb678290d8974ce
LaylaEBT <136167430+LaylaEBT@users.noreply.github.com>
2026-02-08 22:47:28 +0100

    circlegrid + readme

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,38 @@
 #include <SFML/Graphics.hpp>
 
+void drawcircle(sf::RenderWindow & window, float position_x, float position_y)
+{
+ 
+    sf::CircleShape shape(10.f);
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition({ position_x, position_y });
+    window.draw(shape);
+
+}
+
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "SFML works!");
+    
 
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
         }
 
         window.clear();
+        
+        for (float position_x = 0.f; position_x <= 380.f; position_x += 25.f)
+        {
+            for (float position_y = 0.f; position_y <= 380.f; position_y += 25.f)
+            {
+                drawcircle(window, position_x, position_y);
+            }
+        }
         window.display();
     }
+    return 0;
 }
