e6aee1f82f156005bbaa78dcfb09b9ffa1c8bde0
LaylaEBT <136167430+LaylaEBT@users.noreply.github.com>
2026-02-10 15:41:50 +0100

    fix main

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,99 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Main.hpp>
+#include <optional>
+#include <algorithm>
 
-void drawcircle(sf::RenderWindow & window, float position_x, float position_y)
+void drawcircle(sf::RenderWindow & window, float position_x, float position_y, sf::Color color)
 {
  
     sf::CircleShape shape(10.f);
-    shape.setFillColor(sf::Color::Green);
+    shape.setFillColor(color);
     shape.setPosition({ position_x, position_y });
     window.draw(shape);
 
 }
 
+void selectCircle(const sf::Event& event, int& selectedRow, int& selectedCol)
+{
+    if (!event.is<sf::Event::KeyPressed>())
+        return;
+
+
+
+    switch (event.getIf<sf::Event::KeyPressed>()->scancode)
+    {
+    case sf::Keyboard::Scancode::Left:
+    case sf::Keyboard::Scancode::Q: 
+        selectedCol--;
+        break;
+
+    case sf::Keyboard::Scancode::Right:
+    case sf::Keyboard::Scancode::D:
+        selectedCol++;
+        break;
+
+    case sf::Keyboard::Scancode::Up:
+    case sf::Keyboard::Scancode::Z:
+        selectedRow--;
+        break;
+
+    case sf::Keyboard::Scancode::Down:
+    case sf::Keyboard::Scancode::S:
+        selectedRow++;
+        break;
+
+    default:
+        break;
+    }
+}
+
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "SFML works!");
-    
 
-    while (window.isOpen())
+    int selectedRow = 0;
+    int selectedCol = 0;
+
+    const int rows = 16;
+    const int cols = 16;
+    const float spacing = 25.f;
+
+   
+
+    while (window.isOpen()) // boucle moteur
     {
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
+            else
+            {
+                selectCircle(*event, selectedRow, selectedCol);
+            }
         }
 
         window.clear();
-        
-        for (float position_x = 0.f; position_x <= 380.f; position_x += 25.f)
+
+        for (int row = 0; row < rows; ++row)
         {
-            for (float position_y = 0.f; position_y <= 380.f; position_y += 25.f)
+            for (int col = 0; col < cols; ++col)
             {
-                drawcircle(window, position_x, position_y);
+                bool isSelected = (row == selectedRow && col == selectedCol);
+
+                sf::Color color = isSelected
+                    ? sf::Color::Blue
+                    : sf::Color::Green;
+
+                drawcircle(
+                    window,
+                    col * spacing,
+                    row * spacing,
+                    color
+                );
             }
         }
+
         window.display();
     }
     return 0;
